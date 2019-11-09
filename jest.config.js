91d00068c73902e0f59396a5bfb133a22dc6630d
Godwin Malik <despeauxz@gmail.com>
2019-11-09 22:55:49 +0100

    initial commit

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+{
+    // globalSetup: "./src/testUtils/callSetup.js",
+    transform: {
+      "^.+\\.tsx?$": "ts-jest"
+    },
+    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+    moduleFileExtensions: [
+      "ts",
+      "tsx",
+      "js",
+      "jsx",
+      "json",
+      "node"
+    ]
+}
